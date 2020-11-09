9c3dc3dfaf56b77a7e9645efdfdb3d995fc17743
epittab <ep7m@me.com>
2020-11-08 22:20:57 -0500

    added event and marker actions and types for async calls

--- src/Redux/Actions/event.js
+++ src/Redux/Actions/event.js
@@ -1,4 +1,4 @@
-import { CLEANUP_EVENT, CHANGE_EVENT_INFO } from "./type.js";
+import { CLEANUP_EVENT, CHANGE_EVENT_INFO, POST_EVENT_SUCCESS, POST_EVENT_FAILURE, GET_EVENT_SUCCESS, GET_EVENT_FAILURE } from "./type.js";
 
 function changeEventInfo(e){
   return {
@@ -7,12 +7,73 @@ function changeEventInfo(e){
   }
 }
 
-function submitEvent(e){
+function postSuccess(d) {
+  return {
+    type: POST_EVENT_SUCCESS,
+    payload: d
+  }
+}
+
+function postFailure(d) {
+  return {
+    type: POST_EVENT_FAILURE,
+    payload: d
+  }
+}
+
+function submitEvent(e, data){
   e.preventDefault();
   return (dispatch) => {
-    fetch()
-    .then()
-    .then()
+    fetch(`http://localhost:3000/api/v1/events`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then( r => r.json() )
+    .then( d => {
+      if (d.error) {
+        dispatch(postSuccess(d));
+      } else {
+        dispatch(postFailure(d));
+      }
+    })
+  }
+}
+
+function getSuccess(d) {
+  return {
+    type: GET_EVENT_SUCCESS,
+    payload: d
+  }
+}
+
+function getFailure(d) {
+  return {
+    type: GET_EVENT_FAILURE,
+    payload: d
+  }
+}
+
+function getEvents(){
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/events`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then( r => r.json() )
+    .then( d => {
+      if (d.error) {
+        dispatch(getSuccess(d));
+      } else {
+        dispatch(getFailure(d));
+      }
+    })
   }
 }
 
@@ -22,4 +83,4 @@ function cleanupEvent() {
   };
 }
 
-export { changeEventInfo, submitEvent, cleanupEvent };
+export { changeEventInfo, submitEvent, getEvents, cleanupEvent };
--- src/Redux/Actions/marker.js
+++ src/Redux/Actions/marker.js
@@ -1,10 +1,4 @@
-import { ADD_MARKER, CLEANUP_MARKER, CHANGE_MARKER_MENU, CHANGE_MARKER_LOCATION, CHANGE_MARKER_TYPE } from "./type.js";
-
-function addMarker() {
-  return {
-    type: ADD_MARKER,
-  };
-}
+import { CLEANUP_MARKER, CHANGE_MARKER_MENU, CHANGE_MARKER_LOCATION, CHANGE_MARKER_TYPE, POST_MARKER_SUCCESS, POST_MARKER_FAILURE, GET_MARKER_SUCCESS, GET_MARKER_FAILURE  } from "./type.js";
 
 function changeMarkerMenu(view) {
   return {
@@ -27,7 +21,21 @@ function changeMarkerType(e) {
   };
 }
 
-function submitNewMarker(e){
+ function postSuccess(d) {
+  return {
+    type: POST_MARKER_SUCCESS,
+    payload: d
+  }
+}
+
+function postFailure(d) {
+  return {
+    type: POST_MARKER_FAILURE,
+    payload: d
+  }
+}
+
+function submitMarker(e, data){
   e.preventDefault();
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/markers`, {
@@ -36,14 +44,53 @@ function submitNewMarker(e){
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      body: JSON.stringify()
+      body: JSON.stringify(data)
     })
     .then( r => r.json() )
     .then( d => {
-      console.log(d);
+      if (d.error) {
+        dispatch(postSuccess(d));
+      } else {
+        dispatch(postFailure(d));
+      }
     })
   }
- }
+}
+
+function getSuccess(d) {
+  return {
+    type: GET_MARKER_SUCCESS,
+    payload: d
+  }
+}
+
+function getFailure(d) {
+  return {
+    type: GET_MARKER_FAILURE,
+    payload: d
+  }
+}
+
+function getMarkers(){
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/events`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then( r => r.json() )
+    .then( d => {
+      if (d.error) {
+        dispatch(getSuccess(d));
+      } else {
+        dispatch(getFailure(d));
+      }
+    })
+  }
+}
+
 
 function cleanupMarker() {
   return {
@@ -51,4 +98,4 @@ function cleanupMarker() {
   };
 }
 
-export { addMarker, changeMarkerMenu, changeMarkerLocation, changeMarkerType, submitNewMarker, cleanupMarker };
+export { changeMarkerMenu, changeMarkerLocation, changeMarkerType, submitMarker, getMarkers, cleanupMarker };
